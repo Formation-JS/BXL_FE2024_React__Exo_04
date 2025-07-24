f60e3a85805497085e493a86f4ba1cebadd5b6eb
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-07-24 09:53:14 +0200

    Devise - Synchro form et react

--- src/components/currency-convert/currency-convert.jsx
+++ src/components/currency-convert/currency-convert.jsx
@@ -1,26 +1,33 @@
+import { useState } from 'react';
 import { getCurrencyName } from '../../tools/currency.tool';
 import deviseInfo from './../../data/devise.json';
 
 export default function CurrencyConvert() {
 
+    const [currencyFrom, setCurrencyFrom] = useState('EUR');
+    const [valueFrom, setValueFrom] = useState('');
+    const [currencyTo, setCurrencyTo] = useState('CAD');
+    const [valueTo, setValueTo] = useState('');
+
+
     return (
         <form>
             <div>
-                <input type="text" />
-                <select name="">
-                    { deviseInfo.map((elem, idx) => (
+                <input type="text" value={valueFrom} onChange={(e) => setValueFrom(e.target.value)} />
+                <select value={currencyFrom} onChange={(e) => setCurrencyFrom(e.target.value)}>
+                    {deviseInfo.map((elem, idx) => (
                         <option key={idx} value={elem.currency}>{getCurrencyName(elem.currency)}</option>
                     ))}
                 </select>
             </div>
             <div>
-                <input type="text" />
-                 <select name="">
-                    { deviseInfo.map((elem, idx) => (
+                <input type="text" value={valueTo} readOnly />
+                <select value={currencyTo} onChange={(e) => setCurrencyTo(e.target.value)}>
+                    {deviseInfo.map((elem, idx) => (
                         <option key={idx} value={elem.currency}>{getCurrencyName(elem.currency)}</option>
                     ))}
                 </select>
             </div>
         </form>
-    )
+    );
 }
