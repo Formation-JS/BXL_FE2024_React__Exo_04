f2e37779acafe79f88d40d8a7e01379790498ba1
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-07-25 11:23:18 +0200

    ContactForm - Validation with Zod

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -1,14 +1,47 @@
 import { useActionState } from "react";
+import z from "zod";
 
 async function fakeSendToServer(data) {
     await (new Promise((resolve) => setTimeout(resolve, 1_000)));
     console.log('Donnée envoyé !');
 }
 
+const contactSchemaValidation = z.object({
+    email: z.email('Votre email est invalide'),
+    comment: z.string()
+                .max(1_000, 'Votre commentaire est trop long !')
+                .min(1, 'Un commentaire est requis'),
+    condition: z.literal('on').transform((val) => true)
+});
+
 async function contactAction(prevState, formData) {
-    console.log(Object.fromEntries(formData.entries()));
+    const contentFormData = Object.fromEntries(formData.entries());
+    const { data, error, success } = z.safeParse(contactSchemaValidation, contentFormData);
+
+    console.log(data);
+    if (!success) {
+        return {
+            message: 'Erreur dans le formulaire !',
+
+            error: z.treeifyError(error).properties,
+            form: contentFormData
+        };
+    }
+
+    await fakeSendToServer(data);
     
-    return prevState;
+    return {
+        message: 'Demande envoyé !',
+        form: {
+            email: null,
+            comment: null,
+        },
+        error: {
+            email: null,
+            comment: null,
+            condition: null
+        }
+    };
 }
 
 const initialState = {
@@ -33,17 +66,19 @@ export default function ContactForm() {
             <div>
                 <label htmlFor="input-email">Email : </label>
                 <input id="input-email" type="text" name="email" defaultValue={state.form.email} />
-                {state.error.email && <span>{state.error.email}</span>}
+                {state.error.email && <span>{state.error.email.errors.join(', ')}</span>}
             </div>
             <div>
                 <label htmlFor="input-comment">Commentaire : </label>
                 <textarea id="input-comment" name="comment" defaultValue={state.form.comment} />
-                {state.error.comment && <span>{state.error.comment}</span>}
+                {state.error.comment && <span>{state.error.comment.errors.join(', ')}</span>}
             </div>
             <div>
                 <input id="input-condition" type="checkbox" name="condition" />
+                {' '}
                 <label htmlFor="input-condition">Veuillez accepter les conditions général du site.</label>
-                {state.error.condition && <span>{state.error.condition}</span>}
+                {' '}
+                {state.error.condition && <span>{state.error.condition.errors.join(', ')}</span>}
             </div>
             <div>
                 <button type="submit">Envoyer</button>
